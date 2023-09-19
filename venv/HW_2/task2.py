2ea5a73037d3d52bc9137392cfbd6c20c634bd18
bmay <bloodymay456@gmail.com/ssh>
2023-09-19 20:36:23 +0300

    task2_fixed

--- venv/HW_2/task2.py
+++ venv/HW_2/task2.py
@@ -13,10 +13,16 @@ if str_1st_int[1] == str_2nd_int[1]:
      print(f'Результат сложения дробей: {str_1st_int[0] + str_2nd_int[0]}/{str_1st_int[1]}')
 else:
     common_denominator = int(str_1st_int[1] * str_2nd_int[1] / gcd(str_1st_int[1], str_2nd_int[1]))
+    rn = int(common_denominator / str_1st_int[1] * str_1st_int[0] + common_denominator/ str_2nd_int[1] * str_2nd_int[0])
+    g2 = gcd(rn, common_denominator)
+    new_numerator = int(rn / g2)
+    new_denominator = int(common_denominator / g2)
 
-    print(common_denominator)
+    print(f'Результат сложения дробей: {new_numerator}/{new_denominator}')
+
 print(f'Результат умножения дробей: {str_1st_int[0] * str_2nd_int[0]}/{str_1st_int[1] * str_2nd_int[1]}')
 str_1st = '/'.join(str_1st)
 str_2nd = '/'.join(str_2nd)
-print(f'Результат умножения дробей через Fraction: {Fraction(str_1st) * Fraction(str_2nd)} ')
+
 print(f'Результат сложения дробей через Fraction: {Fraction(str_1st) + Fraction(str_2nd)} ')
+print(f'Результат умножения дробей через Fraction: {Fraction(str_1st) * Fraction(str_2nd)} ')
