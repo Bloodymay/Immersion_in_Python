605a04edc400f63140ce84a0087c7a00c4fa1eb1
bmay <bloodymay456@gmail.com/ssh>
2023-10-04 00:23:16 +0300

    task 1 func description added

--- venv/HW_7 (files)/s_task1_rand_numbers.py
+++ venv/HW_7 (files)/s_task1_rand_numbers.py
@@ -4,6 +4,13 @@
 from random import  randint, uniform
 
 def rnd_num_pairs(count:int, file_name: str ):
+    """
+    Генерация рандомных чисел int и float в диапазоне от -1000 до 1000
+    и их запись в файл в формате: int|float, одна пара значений - одна строка
+    :param count:
+    :param file_name:
+    :return:
+    """
     with open(file_name,"a", encoding="utf-8") as f:
         for i in range(count):
             f.write(f"{randint(-1000,1000)}|{uniform(-1000,1000)}\n")
